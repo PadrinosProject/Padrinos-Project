4d84c8406888a284aaa06f655bb89e8519f16038
Lance Meier <lancemeier@gmail.com>
2018-02-26 09:34:28 -0600

    added initial GET routes

--- padrinos-project/routes/index.js
+++ padrinos-project/routes/index.js
@@ -6,4 +6,54 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+router.post('/', (req, res, next) => {
+  res.render ('index');
+});
+
+//Signup
+
+router.get('/signup', (req, res, next) => {
+  res.render('/signup');
+});
+
+//Login
+
+router.get('/login', (req, res, next) => {
+  res.render('/login');
+});
+
+router.post('/login', (req, res, next) => {
+  res.send('logged in!!');
+})
+
+//Logout
+
+router.get('/logout', (req,res,next) => {
+  res.render('/index');
+});
+
+//Create Event
+
+router.get('/create', (req,res,next) => {
+  res.render('/create');
+});
+
+//View Profile
+
+router.get('/view-profile', (req,res,next) => {
+  res.render('/view-profile');
+});
+
+//View Event
+
+router.get('/view-events', (req,res,next) => {
+  res.render('/view-events');
+});
+
+//View Guest Profile
+
+router.get('/view-guest', (req,res,next) => {
+  res.render('/view-guest');
+});
+
 module.exports = router;
