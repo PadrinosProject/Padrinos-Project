f1cd5475a51c59e43246a3fddeac8621ecf10ec2
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-02-26 09:32:25 -0600

    added models directorie, login and signup views

---
+++ padrinos-project/models/Event.js
@@ -0,0 +1,17 @@
+// Event Model
+
+const mongoose = require('mongoose');
+const Schema   = mongoose.Schema;
+
+const EventSchema = Schema({
+  owner:    String, //idUser
+  category: String,
+  date:     Number,
+  guests:  [idUsers],
+  administradores:[idUsers],
+  location: [latitude, longitude]
+});
+
+const Event = mongoose.model('Event', EventSchema);
+
+module.exports = User;
