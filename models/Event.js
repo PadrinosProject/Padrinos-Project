86551349779ab5a0b109d1fb5f3cfb148398ac8d
Marilyn Negrete <negrete.aguilar.m@gmail.com>
2018-08-23 17:37:52 -0500

    cleaned up comments and extra spaces

--- models/Event.js
+++ models/Event.js
@@ -1,23 +1,23 @@
-// Event Model
-
 const mongoose = require('mongoose');
-const Schema   = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const EventSchema = Schema({
+const EventSchema = Schema({  
   owner: Schema.Types.ObjectId,
   name: String,
   category: String,
-  date:     {
+  date: {
     type:Date,
     default: new Date()
-  },
-  // location: [latitude, longitude],
+  }, 
   photo: String,
   description: String,
   guests: [],
   items: []
-}, {
-  timestamps: { createdAt: "created_at", updatedAt: "updated_at" }
+  }, {
+  timestamps: { 
+    createdAt: "created_at",
+    updatedAt: "updated_at" 
+  }
 });
 
 const Event = mongoose.model('Event', EventSchema);
